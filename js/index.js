f17c38b854f1c14cf1a018ba91f48bc5e9b80b04
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2023-02-01 12:51:16 -0700

    add Proper images to slideshow

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 var count = 0;
-setInterval(slideShow, 1000);
+document.querySelector("#img").src = "./images/Pic_1.jfif";
+setInterval(slideShow, 10000);
 function slideShow() {
-    document.querySelector("#img");
     switch (count){
         case 0:
-            document.querySelector("#img").src = "./images/me_irl.jpg";
+            document.querySelector("#img").src = "./images/NerveGear.png";
             count++
             break;
         case 1:
-            document.querySelector("#img").src = "./images/rickroll-roll.gif";
+            document.querySelector("#img").src = "./images/pic_3.jpg";
             count++
             break;
         case 2:
-            document.querySelector("#img").src = "./images/me_irl.jpg";
+            document.querySelector("#img").src = "./images/rickroll-roll.gif";
             count++
             break;
         case 3:
-            document.querySelector("#img").src = "./images/rickroll-roll.gif";
+            document.querySelector("#img").src = "./images/Pic_1.jfif";
             count = 0;
             break;
     }
