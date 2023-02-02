75628e2f95b71c3b01559de52a8715c434cd47a0
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2023-02-02 13:12:04 -0700

    added image api

--- js/contactus.js
+++ js/contactus.js
@@ -8,7 +8,10 @@ fetch("http://quotes.rest/qod.json")
         console.log(data)
         var Quote = document.querySelector("#Quote")
         Quote.textContent = data.contents.quotes[0].quote;
-        document.body.style.backgroundImage = data.contents.quotes[0].background;
+        var img = document.createElement("img")
+        //document.getElementById("myDiv").style.backgroundImage = data.contents.quotes[0].background;
+        img.src = data.contents.quotes[0].background;
+        Quote.appendChild(img);
 
     }
 )
